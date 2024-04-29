0c0d50b6e722fc7bfdb840f4c6d5d7434ae10596
AmitkrMeena02 <amitmeena.meena@yahoo.com>
2024-04-29 21:32:52 +0530

    writing complex JSX

--- project_one/src/Components/ExpenseItem.js
+++ project_one/src/Components/ExpenseItem.js
@@ -1,12 +1,24 @@
 function ExpenseItem(){
-    return(
+  return (
+    <div>
       <div>
-        <p>This is expense item!</p>
-        <p>Food - $5</p>
-        <p>Petrol - $20</p>
-        <p>Movie - $10</p>
+        <h2>Date</h2>
+        <div>15 August 2023</div>
       </div>
-    );
-  }
-  
-  export default ExpenseItem
+      <div>
+        <h2>Title</h2>
+        <div>Book</div>
+      </div>
+      <div>
+        <h2>amount</h2>
+        <div>$10</div>
+      </div>
+      <div>
+        <h2>Location</h2>
+        <div>Delhi</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

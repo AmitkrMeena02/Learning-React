f9573136d7b1a611f3242020d88ea57b1290c960
AmitkrMeena02 <amitmeena.meena@yahoo.com>
2024-04-29 23:23:24 +0530

    styling components

--- project_one/src/Components/ExpenseItem.js
+++ project_one/src/Components/ExpenseItem.js
@@ -1,22 +1,14 @@
-function ExpenseItem(){
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
   return (
-    <div>
-      <div>
-        <h2>Date</h2>
-        <div>15 August 2023</div>
+    <div className='expense-item'>
+      <div className='expense-item_date'>15 August 2023</div>
+      <div className='expense-item__description'>
+        <h2>Book</h2>
+        <div className='expense-item__price'>$10</div>
       </div>
-      <div>
-        <h2>Title</h2>
-        <div>Book</div>
-      </div>
-      <div>
-        <h2>amount</h2>
-        <div>$10</div>
-      </div>
-      <div>
-        <h2>Location</h2>
-        <div>Delhi</div>
-      </div>
+      <div className='expense-item__location'>Delhi</div>
     </div>
   );
 }

125054e43a498edf7bd3054e88958a9ec269bc9e
AmitkrMeena02 <amitmeena.meena@yahoo.com>
2024-04-30 21:09:13 +0530

    props object & passing data to child component

--- project_one/src/App.js
+++ project_one/src/App.js
@@ -4,7 +4,30 @@ function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2024, 7, 15)}
+        location="Bangalore"
+        title="Insurance"
+        price="50"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2024, 3, 25)}
+        location="Delhi"
+        title="Book"
+        price="20"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2024, 8, 22)}
+        location="Hyderabad"
+        title="pen"
+        price="5"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2024, 10, 10)}
+        location="Mumbai"
+        title="Laptop"
+        price="200"
+      ></ExpenseItem>
     </div>
   );
 }

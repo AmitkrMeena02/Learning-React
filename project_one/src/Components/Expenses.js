cf6149ef6b4d140e6ab3dd0f3660fe8b3fd4f1a5
AmitkrMeena02 <amitmeena.meena@yahoo.com>
2024-05-02 12:12:42 +0530

    wrapper components

--- project_one/src/Components/Expenses.js
+++ project_one/src/Components/Expenses.js
@@ -1,41 +1,11 @@
-import "./Expenses.css"
-import ExpenseItem from './ExpenseItem';
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
-function Expenses() {
-    const expenses = [
-        {
-          id: "1",
-          date: new Date(2023, 7, 15),
-          location: "Bangalore",
-          title: "Insurance",
-          price: 100,
-        },
-        {
-          id: "2",
-          date: new Date(2023, 3, 25),
-          location: "Delhi",
-          title: "Book",
-          price: 10,
-        },
-        {
-          id: "3",
-          date: new Date(2023, 10, 11),
-          location: "Hyderabad",
-          title: "Pen",
-          price: 1,
-        },
-        {
-          id: "4",
-          date: new Date(2023, 1, 14),
-          location: "Mumbai",
-          title: "Laptop",
-          price: 200,
-        },
-      ];
-
-    return(
-        <div>
-            {expenses.map((expense) => {
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense, index) => {
         return (
           <ExpenseItem
             key={expense.id}
@@ -43,11 +13,11 @@ function Expenses() {
             location={expense.location}
             title={expense.title}
             price={expense.price}
-          ></ExpenseItem>
+          />
         );
       })}
-        </div>
-    )
+    </Card>
+  );
 }
 
 export default Expenses;

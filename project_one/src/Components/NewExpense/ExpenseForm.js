9d83078eec5e56938e9a9f08efd66ded58c0079d
AmitkrMeena02 <amitmeena.meena@yahoo.com>
2024-05-28 18:19:51 +0530

    multiple to One state

--- project_one/src/Components/NewExpense/ExpenseForm.js
+++ project_one/src/Components/NewExpense/ExpenseForm.js
@@ -2,19 +2,37 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState("");
+    const [userInput, setUserInput] = useState({
+      enteredTitle: "",
+      enteredAmount: "",
+      enteredDate: "",
+    });
+
     const titleChangeHandler = (event) => {
-      setEnteredTitle(event.target.value);
+      setUserInput((prevState) => {
+        return {
+          ...prevState,
+          enteredTitle: event.target.value
+        };
+      });
     };
-  
-    const [enteredAmount, setEnteredAmount] = useState("");
+
     const amountChangeHandler = (event) => {
-      setEnteredAmount(event.target.value);
+      setUserInput((prevState) => {
+        return {
+          ...prevState,
+          enteredAmount: event.target.value
+        };
+      });
     };
-  
-    const [enteredDate, setEnteredDate] = useState("");
+
     const dateChangeHandler = (event) => {
-      setEnteredDate(event.target.value);
+      setUserInput((prevState) => {
+        return {
+          ...prevState,
+          enteredDate: event.target.value
+        };
+      });
     };
 
   return (

d15131c85f5bc4d25d13463c25e5ff5b6b9f4bf0
AmitkrMeena02 <amitmeena.meena@yahoo.com>
2024-08-07 13:12:14 +0530

    adding restaurant summary

--- 04restaurant_app/src/components/Layout/HeaderCartButton.js
+++ 04restaurant_app/src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,12 @@
-//import CartIcon from '../Cart/CartIcon';
+import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   return (
     <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
       <span>Your Cart</span>
       <span className={classes.badge}>0</span>
     </button>
